56d5d37501f9867be062b670ea460188d1048594
Christian Wuerthner <c.wurthner@gmail.com>
2021-02-14 19:12:59 +0000

    Update scripts

---
+++ install.sh
@@ -0,0 +1,32 @@
+
+echo "Welcome!"
+echo "This script will:"
+echo "- Install Docker, Docker Compose and Git"
+echo "- Guide you through the setup for No-IP (DDNS)"
+echo "- Guide you through setting up HTTPS"
+echo
+echo
+echo
+echo
+echo "Let's install Docker, Docker Compose and Git!"
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+sudo docker run hello-world
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo apt-get remove python-configparser
+sudo apt-get install -y git
+sudo pip3 -v install docker-compose
+echo
+echo
+echo
+echo 
+echo "Let's setup Docker Compose to run OctoPrint!"
+cd ~
+git clone .....
+cd octoprint-all-in-one
+echo
+echo
+echo
+echo
+./install.sh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
-echo "Welcome!"
 echo "Let's create a user to secure your OctoPrint"
 read -p "Choose a user name: " USER
 read -sp "Choose a password: " PASSWORD
 echo
 echo "Done! You can change the user and password by running this script again!"
 printf "$USER:$(openssl passwd -crypt $PASSWORD)\n" > nginx/.htpasswd
-
+echo
+echo
+echo
 echo "Let's setup noip so you can reach your OctoPrint remotely"
 sudo docker run -ti -v "${PWD}/noip:/usr/local/etc/" hypriot/rpi-noip noip2 -C
+echo
+echo
+echo
+echo
+echo "Let's generate certificates for HTTPS encryption"
+read -p "Please enter the host name from no-ip [e.g. xxx.ddns.net]: " HOST
+read -p "Please enter your 2-letter country code [e.g. US, DE, NL, ...]: " COUNTRY
+sudo openssl req -x509 -nodes -days 10000 -newkey rsa:2048 -subj "/C=$COUNTRY/CN=$HOST" -addext "subjectAltName = DNS:$HO$
+echo "Last step! This may take up to 15 minutes on older Pi!"
+sudo openssl dhparam -out nginx/certs/dhparam.pem 2048
