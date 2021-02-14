ac68589f05eb04eb00b4511ccb0cf7944a64fdb0
Christian Wuerthner <c.wurthner@gmail.com>
2021-02-14 17:53:17 +0000

    Add no-ip

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ read -sp "Choose a password: " PASSWORD
 echo
 echo "Done! You can change the user and password by running this script again!"
 printf "$USER:$(openssl passwd -crypt $PASSWORD)\n" > nginx/.htpasswd
+
+echo "Let's setup noip so you can reach your OctoPrint remotely"
+sudo docker run -ti -v "${PWD}/noip:/usr/local/etc/" hypriot/rpi-noip noip2 -C
