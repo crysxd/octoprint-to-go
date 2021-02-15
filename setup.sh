c59895db79d545d9b05b4a2628c544800a66d0a1
Christian Würthner <c.wurthner@gmail.com>
2021-02-15 10:16:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ echo "Let's generate certificates for HTTPS encryption"
 read -p "Please enter the host name from no-ip [e.g. xxx.ddns.net]: " HOST
 read -p "Please enter your 2-letter country code [e.g. US, DE, NL, ...]: " COUNTRY
 mkdir -p nginx/certs
-sudo openssl req -x509 -nodes -days 10000 -newkey rsa:2048 -subj "/C=$COUNTRY/CN=$HOST" -addext "subjectAltName = DNS:$HOST" -key nginx/certs/nginx-selfsigned.key -out nginx/certs/nginx-selfsigned.crt
+sudo openssl req -x509 -nodes -days 10000 -newkey rsa:2048 -subj "/C=$COUNTRY/CN=$HOST" -addext "subjectAltName = DNS:$HOST" -keyout nginx/certs/nginx-selfsigned.key -out nginx/certs/nginx-selfsigned.crt
 sed -i "s/%%%HOST%%%/$HOST/g" nginx/conf.d/octoprint.conf
 echo "Last step! This may take up to 15 minutes on older Pi!"
 sudo openssl dhparam -out nginx/certs/dhparam.pem 2048
